cc37701f6f18c64c986bb1c5a59c47bb7ac9aebd
Keller Beckstead <kellerjbeckstead@gmail.com>
2021-10-31 11:08:35 -0600

    finished task manager

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const connectDB = require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handling')
 require('dotenv').config();
 
 //middleware
@@ -17,8 +19,10 @@ app.get('/hello', (req, res) => {
 })
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
    try {
